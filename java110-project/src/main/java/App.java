713ca5bbe81bb6726d50a4fc3ad099b828ae5cc3
lee <kei_romance@naver.com>
2018-09-05 11:05:12 +0900

    강사관리 기능추가

--- java110-project/src/main/java/App.java
+++ java110-project/src/main/java/App.java
@@ -59,12 +59,41 @@ public class App {
        
    }
    
+   static class Teacher extends Member{
+       protected String tel;
+       protected int pay;
+       protected String subjects;
+    public String getTel() {
+        return tel;
+    }
+    public void setTel(String tel) {
+        this.tel = tel;
+    }
+    public int getPay() {
+        return pay;
+    }
+    public void setPay(int pay) {
+        this.pay = pay;
+    }
+    public String getSubjects() {
+        return subjects;
+    }
+    public void setSubjects(String subjects) {
+        this.subjects = subjects;
+    }
+       
+       
+       
+   }
+   
    
     
     static Student[] students = new Student[100];
+    static Teacher[] teachers = new Teacher[100];
     
     
-    static int index = 0;
+    static int studentIndex = 0;
+    static int teacherIndex = 0;
     
     static Scanner keyIn = new Scanner(System.in);
    
@@ -76,9 +105,14 @@ public class App {
         
         if(menu.equals("1")) {
             serviceStudentMenu();
+            
+        }else if(menu.equals("2")) {
+            serviceTeacherMenu();
+            
        } else if(menu.equals("0")){
            System.out.println("안녕히 가세요!");
            break;
+           
        }
         }
         
@@ -96,6 +130,27 @@ public class App {
             
         }else if(command.equals("add")) {
             inputStudents();
+        
+        }else if(command.equals("quit")) {
+            break;
+            
+        }else {
+            System.out.println("유효하지 않는 명령입니다.");
+            
+        }
+      }
+    }
+    
+    private static void serviceTeacherMenu() {
+        while(true) {
+        System.out.println("강사 관리> ");
+        String command = keyIn.nextLine();
+        
+        if(command.equals("list")) {
+            printTeachers();
+            
+        }else if(command.equals("add")) {
+            inputTeachers();
         
         }else if(command.equals("quit")) {
             break;
@@ -138,7 +193,7 @@ public class App {
         int count = 0;
     
         for(Student s : students) {// 배열이나 컬렉션이 들어감
-            if(count++ == index)// 증가된 값과 비교하는게 아니다
+            if(count++ == studentIndex)// 증가된 값과 비교하는게 아니다
                 break;
             System.out.printf("%s, %s, %s, %s, %b, %s\n"
                     , s.getName()
@@ -147,6 +202,24 @@ public class App {
                     , s.getSchool()
                     , s.isWorking()
                     , s.getTel());
+            
+            }
+             
+    }
+    
+    static void printTeachers() {
+        int count = 0;
+    
+        for(Teacher s : teachers) {// 배열이나 컬렉션이 들어감
+            if(count++ == teacherIndex)// 증가된 값과 비교하는게 아니다
+                break;
+            System.out.printf("%s, %s, %s, %s, %d, [%s]\n"
+                    , s.getName()
+                    , s.getEmail()
+                    , s.getPassword()
+                    , s.getTel()
+                    , s.getPay()
+                    , s.getSubjects());
             
             }
              
@@ -174,7 +247,45 @@ public class App {
             System.out.print("재직여부? (true/false) ");
             m.setWorking(Boolean.parseBoolean(keyIn.nextLine()));
             
-            students[index++] = m;// 현재 index 값을 이 자리에 넣기(후위 연산자)
+            students[studentIndex++] = m;// 현재 index 값을 이 자리에 넣기(후위 연산자)
+             
+            System.out.print("계속하시겠습니까? (Y/n) ");// 대문자 Y는 default값이 Y라는 것
+            String answer = keyIn.nextLine();
+            if(answer.toLowerCase().equals("n")) {
+                
+                break;
+                
+            }
+            
+        }
+        
+    }
+    
+static void inputTeachers() {
+        
+        while(true) {
+            Teacher m = new Teacher();
+            
+            
+            System.out.print("이름? ");
+            m.setName(keyIn.nextLine());
+            
+            System.out.print("이메일? ");
+            m.setEmail(keyIn.nextLine());
+            
+            System.out.print("암호? ");
+            m.setPassword(keyIn.nextLine());
+            
+            System.out.print("전화번호? ");
+            m.setTel(keyIn.nextLine());
+            
+            System.out.print("시급? ");
+            m.setPay(Integer.parseInt(keyIn.nextLine()));
+            
+            System.out.print("과목? ");
+            m.setSubjects(keyIn.nextLine());
+            
+            teachers[teacherIndex++] = m;// 현재 index 값을 이 자리에 넣기(후위 연산자)
              
             System.out.print("계속하시겠습니까? (Y/n) ");// 대문자 Y는 default값이 Y라는 것
             String answer = keyIn.nextLine();
