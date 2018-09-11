9c10cbcff15ee3b845f184c479af2845c3244752
lee <kei_romance@naver.com>
2018-09-11 16:50:49 +0900

    전부 수정 완료

---
+++ java110-project/src/main/java/bitcamp/java110/cms/dao/TeacherFileDao.java
@@ -0,0 +1,114 @@
+package bitcamp.java110.cms.dao;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.util.ArrayList;
+import java.util.List;
+
+import bitcamp.java110.cms.annotation.Component;
+import bitcamp.java110.cms.domain.Teacher;
+
+@Component
+public class TeacherFileDao implements TeacherDao {
+
+    private List<Teacher> list = new ArrayList<>();
+
+    public TeacherFileDao() {
+        File dataFile = new File("data/teacher.dat");
+
+        try(BufferedReader in = new BufferedReader(new FileReader(dataFile));){
+           
+
+            while(true) {
+                String line = in.readLine();
+                if(line == null)
+                    break;
+
+                String[] values = line.split(",");
+
+                Teacher s = new Teacher();
+                s.setName(values[0]);
+                s.setEmail(values[1]);
+                s.setPassword(values[2]);
+                s.setSubjects(values[2]);;
+                s.setTel(values[4]);
+                s.setPay(Integer.parseInt(values[5]));
+
+                list.add(s);
+
+
+            }
+
+        }catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void save() {
+        File dataFile = new File("data/teacher.dat");
+
+        try(BufferedWriter out = new BufferedWriter(new FileWriter(dataFile));){
+
+
+            for(Teacher s: list) {
+                out.write(String.format("%s, %s, %s, %s, %s, %d\n"
+                        ,s.getName()
+                        ,s.getEmail()
+                        ,s.getPassword()
+                        ,s.getSubjects()
+                        ,s.getTel()
+                        ,s.getPay()));
+
+            }
+            out.flush();
+        }catch(Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public int insert(Teacher teacher) {
+
+        for(Teacher item : list) {
+            if(item.getEmail().equals(teacher.getEmail())) {
+                return 0;
+            }
+        }
+        list.add(teacher);
+        save();
+        return 1;
+
+
+    }
+
+    public List<Teacher> findAll() {
+        return list;
+
+    }
+
+    public Teacher findByEmail(String email) {
+        for(Teacher item : list) {
+            if(item.getEmail().equals(email)) {
+                return item;
+            }
+        }
+        return null;
+
+    }
+
+    public int delete(String email) {
+        for(Teacher item : list) {
+            if(item.getEmail().equals(email)) {
+                list.remove(item);
+                return 1;
+            }
+        }
+        return 0;
+
+    }
+
+}// end class
+
---
+++ java110-project/src/main/java/bitcamp/java110/cms/dao/TeacherListDao.java
@@ -0,0 +1,52 @@
+package bitcamp.java110.cms.dao;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import bitcamp.java110.cms.annotation.Component;
+import bitcamp.java110.cms.domain.Teacher;
+
+@Component
+public class TeacherListDao {
+    
+    private List<Teacher> list = new ArrayList<>();
+    
+    public int insert(Teacher teacher) {
+        for(Teacher item: list) {
+            if(item.getEmail().equals(teacher.getEmail())) {
+                return 0;
+                
+            }
+        }
+        return 1;
+        
+    }
+    
+    public List<Teacher> findAll() {
+        return list;
+        
+    }
+    
+    public Teacher findByEmail(String email) {
+        for(Teacher item: list) {
+            if(item.getEmail().equals(email)) {
+                return item;
+            }
+        }
+        return null;
+        
+    }
+    
+    public int delete(String email) {
+        for(Teacher item: list) {
+            if(item.getEmail().equals(email)) {
+                list.remove(item);
+                return 1;
+                
+            }
+        }
+        return 0;
+        
+    }
+
+}// end class
