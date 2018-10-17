8b4da692b68adf5daabc34ede99561279bc4fe58
lee <kei_romance@naver.com>
2018-10-17 19:07:03 +0900

    spring 시작

---
+++ java110-spring-ioc/src/main/java/ex01/CarFactory2.java
@@ -0,0 +1,40 @@
+package ex01;
+
+import java.sql.Date;
+
+public class CarFactory2 {
+    
+    public Car create(String model) {
+        Car c = new Car();
+        switch(model) {
+        case "티코":
+            c.setModel("Tico");
+            c.setCc(800);
+            c.setMaker("대우자동차");
+            c.setCreatedDate(new Date(System.currentTimeMillis()));// 자바 버추얼 머신 -> System
+            break;
+
+        case "소나타":
+            c.setModel("Sonata");
+            c.setCc(1980);
+            c.setMaker("현대자동차");
+            c.setCreatedDate(new Date(System.currentTimeMillis()));// 자바 버추얼 머신 -> System
+            break;
+
+        case "그랜져":
+            c.setModel("grandeur");
+            c.setCc(2000);
+            c.setMaker("현대자동차");
+            c.setCreatedDate(new Date(System.currentTimeMillis()));
+            break;
+
+        default:
+            c.setModel("Avante");
+            c.setCc(1500);
+            c.setMaker("현대자동차");
+            c.setCreatedDate(new Date(System.currentTimeMillis()));
+
+        }
+        return c;
+    }
+}
